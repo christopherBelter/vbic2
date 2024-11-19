f66ca895d4faa6fbbbb329884d417fa703295910
Christopher Belter <christopher.belter@nih.gov>
2024-11-19 16:10:25 -0500

    Add files via upload

--- vbic_session_code.r
+++ vbic_session_code.r
@@ -1,3 +1,4 @@
+# Vignette 1
 source("qvr_processing.r")
 
 appls <- read.csv("hd_admin_fy25_noText.csv", stringsAsFactors = FALSE)
@@ -21,3 +22,24 @@ vbic_out <- vbic_classify(appls, tterms, doc_id_column = "appl_id", doc_text_col
 appls <- merge(appls, vbic_out$concept_list[,c(1,3,4)], by.x = "appl_id", by.y = "doc_id", all.x = TRUE)
 
 write.csv(appls, file = "clustered_applications.csv", row.names = FALSE)
+
+
+# Vignette 2
+source("vbic_classify.r")
+
+source("get_nih_reporter.r")
+
+mq <- create_query(IC = "NICHD", include_active = TRUE, exclude_subprojects = TRUE)
+
+grants <- get_nih_reporter(mq, "nichd_active.txt")
+
+grants$dtext <- paste(grants$project_title, grants$abstract_text, grants$phr_text, grants$project_title, grants$project_title)
+
+tterms <- read.csv("nichd_theme_terms2.csv", stringsAsFactors = FALSE, allowEscapes = TRUE)
+tterms <- tterms[grepl("theme|ncmrr", tterms$concept),]
+
+vbic_out <- vbic_classify(grants, tterms, concept_threshold = 50, doc_id_column = "appl_id", doc_text_column = "dtext")
+
+grants <- merge(grants, vbic_out$concept_list[,c(1,3,4)], by.x = "appl_id", by.y = "doc_id", all.x = TRUE)
+
+write.csv(grants, file = "clustered_grants.csv", row.names = FALSE)
